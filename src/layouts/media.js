07eff1fcec8dc13174824238a6f0ed9e1f027225
fujisawakyle <fujisawakyle@gmail.com>
2018-03-05 09:50:00 -0800

    add media.js

---
+++ src/layouts/media.js
@@ -0,0 +1,23 @@
+import { css } from 'styled-components';
+
+const sizes = {
+  xl: 1200,
+  lg: 992,
+  md: 790,
+  sm: 550,
+  xs: 420
+};
+
+const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16;
+  accumulator[label] = (...args) => css`
+    @media (min-width: ${emSize}em) {
+      ${css(...args)};
+    }
+  `;
+  return accumulator;
+}, {});
+
+export default media;
