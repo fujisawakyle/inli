72232cdf295bf3ace7f11470fd4c9172ac21c8e9
fujisawakyle <fujisawakyle@gmail.com>
2018-03-18 07:30:49 -0700

    add margin so HeroVideo no longer under header

--- src/components/HeroVideo/HeroVideo.js
+++ src/components/HeroVideo/HeroVideo.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { RespVideo } from './HeroVideo.style';
+import { Container, RespVideo } from './HeroVideo.style';
 
 export default class HeroVideo extends Component {
   state = {};
 
   render() {
     return (
-      <RespVideo>
-        <iframe
-          width="560"
-          height="315"
-          src={this.props.videoURL}
-          frameBorder="0"
-          allowFullScreen
-        />
-      </RespVideo>
+      <Container>
+        <RespVideo>
+          <iframe
+            width="560"
+            height="315"
+            src={this.props.videoURL}
+            frameBorder="0"
+            allowFullScreen
+          />
+        </RespVideo>
+      </Container>
     );
   }
 }
--- src/components/HeroVideo/HeroVideo.style.js
+++ src/components/HeroVideo/HeroVideo.style.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
+import media from '../../layouts/media';
+
+export const Container = styled.div`
+  margin-top: 4em;
+  ${media.md`
+    margin-top: 3em;
+  `};
+`;
 
 export const RespVideo = styled.div`
   width: 100%;
   position: relative;
   padding-bottom: 56.25%;
-
   height: 0;
   margin-right: -1px; // hack away strange 1px empty space on right of video
   overflow: hidden;
