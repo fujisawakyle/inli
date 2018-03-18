39725d758efe4d203b60950f02c61738633554d0
fujisawakyle <fujisawakyle@gmail.com>
2018-03-18 07:31:55 -0700

    update styles and content for McNuggetSection

--- src/components/McNuggetSection/McNuggetSection.js
+++ src/components/McNuggetSection/McNuggetSection.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import ModalTrigger from '../Modal/ModalTrigger';
-import { Container, SmallText, LargeText } from './McNuggetSection.style';
+import {
+  Container,
+  ContentContainer,
+  ImageContainer,
+  Image,
+  Title,
+  Subtitle
+} from './McNuggetSection.style';
+
+import mcNuggetImg from '../../assets/images/campaigns-mcdonalds-website-chicken-nugget-small.png';
 
 export default class McNuggetSection extends Component {
   state = {};
 
   render() {
     return (
-      <Container background={this.props.background}>
-        <div>
-          <SmallText>{this.props.first}</SmallText>
-          <LargeText>{this.props.second}</LargeText>
-          <SmallText>{this.props.third}</SmallText>
+      <Container>
+        <ContentContainer>
+          <Title>{this.props.first}</Title>
+          <Subtitle>{this.props.second}</Subtitle>
           <ModalTrigger
             bgColor="orange"
             modalTransition="scaleUp"
             triggerItem={this.props.triggerItem}
             markupToDisplay={this.props.markupToDisplay}
           />
-        </div>
+        </ContentContainer>
+        <ImageContainer imgSm={this.props.imgSm}>
+          <Image src={this.props.imgSm} />
+        </ImageContainer>
       </Container>
     );
   }
--- src/components/McNuggetSection/McNuggetSection.style.js
+++ src/components/McNuggetSection/McNuggetSection.style.js
@@ -1,22 +1,67 @@
 import styled from 'styled-components';
+import media from '../../layouts/media';
 
 export const Container = styled.div`
-  background: url(${props => props.background});
-  background-position: center center;
+  background: #edeef0;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 1rem 0;
+  padding-top: 2.5em;
+  ${media.md`
+    flex-direction: row;
+    padding: 3em 0;
+`};
 `;
 
-export const SmallText = styled.h4`
-  color: white;
+export const ContentContainer = styled.div`
+  color: #222222;
+  width: 95%;
+
+  ${media.sm`
+    width: 50%;
+  `} ${media.md`
+    max-width:400px;
+    order: 1;
+  `};
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  position: relative;
+  max-width: 600px;
+
+  ${media.md`
+    width: 50%;
+    max-width: 400px;
+    height: 250px;
+  `};
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  position: relative;
+
+  ${media.md`
+    position: absolute;
+    left: -33%;
+    top: -92px;
+    width: 144%;
+    max-width: 600px;
+  `};
+`;
+
+export const Title = styled.h1`
+  text-transform: uppercase;
   text-align: center;
   margin-bottom: 1rem;
+  font-weight: 900;
+  ${media.md`
+    font-size: 3rem;
+  `};
 `;
-export const LargeText = styled.h1`
-  color: white;
-  text-transform: uppercase;
+
+export const Subtitle = styled.h4`
   text-align: center;
   margin-bottom: 1rem;
 `;
