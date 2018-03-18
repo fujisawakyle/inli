b5e0822ebb2d7d1aa6f79c0fad6a3077efce36a1
fujisawakyle <fujisawakyle@gmail.com>
2018-03-18 07:32:21 -0700

    update styles and content for RonaldSection

--- src/components/RonaldSection/RonaldSection.js
+++ src/components/RonaldSection/RonaldSection.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import ModalTrigger from '../Modal/ModalTrigger';
-import { Container, SmallText, LargeText } from './RonaldSection.style';
+import {
+  Container,
+  ContentContainer,
+  ImageContainer,
+  Image,
+  Title,
+  Subtitle
+} from './RonaldSection.style';
+
+import ronaldImgSm from '../../assets/images/campaigns-mcdonalds-website-ronald-small.png';
 
 export default class RonaldSection extends Component {
   state = {};
 
   render() {
     return (
-      <Container background={this.props.background}>
-        <div>
-          <SmallText>{this.props.first}</SmallText>
-          <LargeText>{this.props.second}</LargeText>
-          <SmallText>{this.props.third}</SmallText>
+      <Container>
+        <ContentContainer>
+          <Title>{this.props.first}</Title>
+          <Subtitle>{this.props.second}</Subtitle>
           <ModalTrigger
             modalTransition="scaleUp"
             triggerItem={this.props.triggerItem}
             markupToDisplay={this.props.markupToDisplay}
           />
-        </div>
+        </ContentContainer>
+        <ImageContainer imgSm={this.props.imgSm}>
+          <Image src={ronaldImgSm} />
+        </ImageContainer>
       </Container>
     );
   }
--- src/components/RonaldSection/RonaldSection.style.js
+++ src/components/RonaldSection/RonaldSection.style.js
@@ -1,22 +1,68 @@
 import styled from 'styled-components';
+import media from '../../layouts/media';
 
 export const Container = styled.div`
-  background: url(${props => props.background});
-  background-position: center center;
+  background: #222222;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 2.5em;
+  ${media.md`
+    flex-direction: row;
+`};
+`;
+
+export const ContentContainer = styled.div`
+  color: white;
+  width: 95%;
+
+  ${media.sm`
+    width: 45%;
+  `};
+
+  ${media.md`
+    width: 50%;
+    max-width: 400px;
+  `};
+`;
+
+export const ImageContainer = styled.div`
+  width: 95%;
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 1rem 0;
+
+  ${media.md`
+    width: 50%;
+    max-width: 400px;
+  `};
 `;
 
-export const SmallText = styled.h4`
-  color: white;
-  text-align: center;
-  margin-bottom: 1rem;
+export const Image = styled.img`
+  width: 70%;
+  margin-top: 1em;
+  max-width: 400px;
+
+  ${media.md`
+    width: 100%;
+  `};
 `;
-export const LargeText = styled.h1`
-  color: white;
+
+export const Title = styled.h1`
   text-transform: uppercase;
   text-align: center;
   margin-bottom: 1rem;
+  font-weight: 900;
+
+  ${media.md`
+    font-size: 3rem;
+  `};
+`;
+
+export const Subtitle = styled.h4`
+  text-align: center;
+  max-width: 265px;
+  margin: 1rem auto;
 `;
