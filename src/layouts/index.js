2ec9a38f3d8540c098405c0796de60a6c5391845
fujisawakyle <fujisawakyle@gmail.com>
2018-03-24 09:20:46 -0700

    add test og meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,10 +6,18 @@ import './index.css';
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="I'm Not Lovin' it"
+      title="I'm Not Lovin' It"
       meta={[
-        { name: 'description', content: "I'm not lovin' it website" },
-        { name: 'keywords', content: 'sample, something' }
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+        { property: 'og:title', content: "I'm Not Lovin' It" },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://imnotlovinit.com' },
+        {
+          property: 'og:image',
+          content:
+            'https://s3.amazonaws.com/the-humane-league-homepage/THL17-comms-share-image.jpg'
+        }
       ]}
     />
     <div>{children()}</div>
