82bf2d26630054ef9cedd72019a1e74bce0d20d4
fujisawakyle <fujisawakyle@gmail.com>
2018-03-24 20:33:39 -0700

    fix email encode/decode bug

--- src/components/ActionsBar/Action.js
+++ src/components/ActionsBar/Action.js
@@ -14,9 +14,8 @@ import {
   EmailBody,
   FacebookPost,
   CopyButton,
-  Button,
+  Button
 } from './Action.style';
-
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default class Action extends Component {
@@ -33,13 +32,13 @@ export default class Action extends Component {
     this.setState({ [type]: e.target.value });
   };
 
-  onCopy = (id) => {
+  onCopy = () => {
     this.setState({ copied: true });
-    if (document.getElementById(id)) {
-      document.getElementById(id).style.display = 'inline-block';
+    if (document.getElementById('copyButton')) {
+      document.getElementById('copyButton').style.display = 'inline-block';
     }
     setTimeout(() => {
-      document.getElementById(id).style.display = 'none';
+      document.getElementById('copyButton').style.display = 'none';
     }, 1000);
   };
 
@@ -55,7 +54,7 @@ export default class Action extends Component {
         );
         break;
       case '3':
-        let body = encodeURIComponent(this.state.emailBodyValue);
+        let body = encodeURIComponent(decodeURIComponent(this.state.emailBodyValue));
         window.open(
           `mailto:${this.state.emailRecipientValue}?subject=${
             this.state.emailSubjectValue
@@ -63,7 +62,7 @@ export default class Action extends Component {
         );
         break;
       case '4':
-        window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A//imnotlovinit.com');
+        window.open('https://facebook.com/thehumaneleague');
     }
   };
   render() {
@@ -98,36 +97,26 @@ export default class Action extends Component {
       case '3':
         html = (
           <ActionFields>
-            <ActionInputLabel>To</ActionInputLabel>
+            <ActionInputLabel> To </ActionInputLabel>
             <EmailRecipient
               type="text"
               value={this.state.emailRecipientValue}
               onChange={e => this.handleChange(e, 'emailRecipientValue')}
             />
+            <br />
             <ActionInputLabel> Subject </ActionInputLabel>
             <EmailSubject
               type="text"
               value={this.state.emailSubjectValue}
               onChange={e => this.handleChange(e, 'emailSubjectValue')}
             />
+            <br />
             <ActionInputLabel> Message </ActionInputLabel>
             <EmailBody
               type="text"
               value={decodeURIComponent(this.state.emailBodyValue)}
               onChange={e => this.handleChange(e, 'emailBodyValue')}
             />
-            <CopyToClipboard
-              className="copy-btn copy-btn--email copy-btn--emailBody"
-              onCopy={this.onCopy.bind(this, 'confirmBody')}
-              text={this.state.emailBodyValue}
-            >
-              <CopyButton>Copy</CopyButton>
-            </CopyToClipboard>
-            {this.state.copied ? (
-              <div id="confirmBody" className="copyConfirm copyConfirm--emailBody">
-                Copied.
-              </div>
-            ) : null}
             <br />
           </ActionFields>
         );
@@ -135,6 +124,23 @@ export default class Action extends Component {
       case '4':
         html = (
           <ActionFields>
+            <FacebookPost
+              type="text"
+              value={this.state.facebookValue}
+              onChange={e => this.handleChange(e, 'facebookValue')}
+            />
+            <br />
+            <CopyToClipboard
+              onCopy={this.onCopy}
+              text={this.state.facebookValue}
+            >
+              <CopyButton>Copy</CopyButton>
+            </CopyToClipboard>
+            {this.state.copied ? (
+              <span id="copyButton" style={{ color: 'white' }}>
+                Copied.
+              </span>
+            ) : null}
             <br />
           </ActionFields>
         );
